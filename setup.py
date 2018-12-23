5ba778217c5cebbd8d418bf5bb9879e88e02b1b3
Lars Tingelstad <lars.tingelstad@ntnu.no>
2018-12-23 17:51:35 +0100

    move deps to setup.py and remove requirements.txt

--- setup.py
+++ setup.py
@@ -6,28 +6,34 @@
 
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 setup(name='tpk4170',
       version='0.0.2',
       description='Python modules for the course TPK4170 Robotics at NTNU ',
       author='Lars Tingelstad',
       author_email='lars.tingelstad@ntnu.no',
       url='',
+      download_url='https://github.com/tingelst/tpk4170-robotics/archive/master.zip',
       packages=find_packages(),
-      install_requires=requirements,
+      install_requires=[
+          'numpy>=1.15.0',
+          'matplotlib>=3.0.0',
+          'pycollada>=0.4.1',
+          'pythreejs>=2.0.0',
+          'opencv-python>=3.4.4',
+          'mypy>=0.630',
+          'typing_extensions>=3.6.6'
+      ],
       data_files=[
           ('share/tpk4170/models/ur5/visual/',
-             [
-                 'tpk4170/models/ur5/visual/base.dae',
-                 'tpk4170/models/ur5/visual/forearm.dae',
-                 'tpk4170/models/ur5/visual/shoulder.dae',
-                 'tpk4170/models/ur5/visual/upperarm.dae',
-                 'tpk4170/models/ur5/visual/wrist1.dae',
-                 'tpk4170/models/ur5/visual/wrist2.dae',
-                 'tpk4170/models/ur5/visual/wrist3.dae',
-             ]),
+           [
+               'tpk4170/models/ur5/visual/base.dae',
+               'tpk4170/models/ur5/visual/forearm.dae',
+               'tpk4170/models/ur5/visual/shoulder.dae',
+               'tpk4170/models/ur5/visual/upperarm.dae',
+               'tpk4170/models/ur5/visual/wrist1.dae',
+               'tpk4170/models/ur5/visual/wrist2.dae',
+               'tpk4170/models/ur5/visual/wrist3.dae',
+           ]),
           ('share/tpk4170/models/kr6r900sixx/visual/',
            [
                'tpk4170/models/kr6r900sixx/visual/base_link.dae',
